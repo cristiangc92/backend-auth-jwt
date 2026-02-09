987b09fea099f69aeafa1851b8aef27f2d6d1e80
Cristian Cacciolatti <cristiangc92@gmail.com>
2026-02-08 23:23:05 -0300

    Implementa login de usuarios y generación de JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import pool from "../db/index.js"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     try {
@@ -42,6 +43,65 @@ export const register = async (req, res) => {
         console.error(error)
         res.status(500).json({
             message: "Error registrado usuario"
+        })
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        //Validaciones
+        if(!email || !password){
+            return res.status(400).json({
+                message: "Email y contraseña son obligatorios"
+            })
+        }
+
+        //Buscar usuario
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        )
+
+        if(result.rows.length === 0){
+            return res.status(401).json({
+                message: "Credenciales invalidas"
+            })
+        }
+
+        const user = result.rows[0]
+
+        //Comparar contraseñas
+        const isValidPassword = await bcrypt.compare(password, user.password)
+
+        if(!isValidPassword){
+            return res.status(401).json({
+                message: "Credenciales invalidas"
+            })
+        }
+
+        //Generar token
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+
+        //Respuesta
+        res.json({
+            message: "Login exitoso",
+            token
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: "Error al iniciar sesion"
         })
     }
 }
